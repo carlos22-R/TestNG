6a5f070bf37c8bd718a360c035923ea8d7131e0a
Figuer@ <alex.fuge503@gmail.com>
2024-11-26 16:06:16 -0600

    Abstract component and contructor

---
+++ src/main/java/com/claro/abstractcomponent/AbstractComponent.java
@@ -0,0 +1,19 @@
+package com.claro.abstractcomponent;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class AbstractComponent {
+
+  WebDriver driver;
+
+  public AbstractComponent(WebDriver driver){
+
+    this.driver = driver;
+    PageFactory.initElements(driver, this);
+  }
+
+
+
+
+}
